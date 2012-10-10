7a61d2f1591a8f8ff7667e5135b74e2fedbe8915
Gregor Smith <gregor@gregorsmith.com>
2012-10-10 20:10:54 +0100

    Lifestream source code

---
+++ index.php
@@ -0,0 +1,76 @@
+<?php
+require(ABSPATH . 'wp-blog-header.php');
+require_once( ABSPATH . WPINC . '/rss-functions.php' );
+include_once( 'life.php' );
+ini_set( "display_errors", 0);
+?>
+
+      <table border="0" cellpadding="0" cellspacing="0" class="hcalendar">
+        <tbody>
+          <?php
+date_default_timezone_set("America/New_York");
+define('MAGPIE_CACHE_AGE', 10*10); // ten minutes, I think.
+
+$details = array('title','link');
+$list = array();
+
+foreach ( life::defaultStreams() as $name => $feed ) {
+	$rss = @fetch_rss( $feed );
+			$items = $rss->items = array_slice( $rss->items, 0 );
+			
+			foreach ($items as $item ) {
+				$date = strtotime( substr( $item['pubdate'], 0, 25 ) );
+				if( $name == "life_flickr") {
+				$date += 3600*13;
+				}
+				if( $name == "life_twitter") {
+				$date += 3600*6;
+				}
+				if( $name == "life_lastfm") {
+				$date += 3600*6;
+				}
+				if( $name == "life_foursquare") {
+				$date += 3600*13;
+				}
+				if( $name == "life_facebook") {
+				$date += 3600*6;
+				}
+				if( $name == "life_blog") {
+				$date += 3600*1;
+				}
+				else {
+				$date -= 3600*5;
+				}
+				$list[ $date ][ "name" ] = $name;
+				
+				foreach ($details as $detail) {
+					$list[$date]['title'] = $item['title'];
+
+					$list[$date]['link'] = $item['link'];
+		        }
+			}
+}
+krsort( $list );
+$day = '';
+foreach ( $list as $timestamp => $item ) {
+    $this_day = date("F jS",$timestamp );
+    	if ( $day != $this_day ) {
+?>
+        </tbody>
+        <thead>
+          <tr>
+            <th colspan="3" align="left"> <br/>
+              <h2><?php echo $this_day; ?></h2></th>
+          </tr>
+        </thead>
+        </tbody>
+        
+        <?php $day = $this_day; } ?>
+        <tr class="<?php  echo $item["name"]; ?>" />
+        <td><img src="<?php echo 'images/' . $item["name"] . '.gif'; ?>" alt="<?php echo $item["name"]; ?>" /></td>
+          <th><?php echo date("g:ia",$timestamp); ?></th>
+          <td><a class="url summary" href="<?php echo $item["link"]; ?>" title="<?php echo $item["title"]; ?>"><?php echo $item["title"]; ?></a> </td>
+        </tr>
+        <?php } ?>
+        </tbody>
+      </table>
---
+++ life.php
@@ -0,0 +1,59 @@
+<?php
+
+include_once (ABSPATH . 'wp-blog-header.php');
+
+class life {
+
+function defaultStreams() {
+	// To add another service to the system, simply
+	// add it to this array().
+	return array( 
+		'life_blog' => '',
+		'life_flickr' => '',
+		'life_twitter' => '',
+		'life_facebook' => '',
+		'life_lastfm' => '',
+		'life_foursquare' => '',
+		);
+}
+
+function checkStreams() {
+	if( get_option( 'life_installed' ) ) {
+	} else {
+		self::install();
+	}
+}
+
+function install() {
+	add_option( 'life_installed', '1', "" );
+	add_option( 'life_version', '1.0', "");
+	foreach ( self::defaultStreams() as $key => $value ) {
+		add_option( $key, $value, "" );
+	}
+	echo 'Installed, ready to go.';
+}
+
+function upgrade() {
+	foreach ( self::defaultStreams() as $key => $value ) {
+		if( !get_option( $key ) ) {
+		add_option( $key, $value, "" );
+		echo 'Added ' . $key;
+		}
+	}
+}
+
+function loadStreams() {
+	foreach( self::defaultStreams() as $key => $value ) {
+	// find the values for each of our streams (RSS Feeds)
+		if( get_option( $value ) ) {
+			$value = get_option(  $value );
+		} else {
+			echo '<p>' . $key . ': ' . $value . ' not found in the database.</p>';
+		}
+	}
+}
+}
+
+life::checkStreams();
+
+?>
